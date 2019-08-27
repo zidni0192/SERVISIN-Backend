6ad8d7836dd70b9f4ac83f76283309e0531c11f7
falihnaufal17 <dudu.kotkot@gmail.com>
2019-08-27 11:06:46 +0700

    add response

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,15 +1,25 @@
 const crypto = require('crypto')
 module.exports = {
-    generateSalt: () => {
-        return crypto.randomBytes(18/2).toString('hex').slice(0,18)
+    response: (res, result, status, error) => {
+        let resultPrint = {}
+
+        resultPrint.error = error || null
+        resultPrint.status_code = status || 200
+        resultPrint.result = result
+
+        return res.status(resultPrint.status_code).json(resultPrint)
     },
-    setPassword: (password,salt) =>{
-        let hash = crypto.createHmac('sha512',salt)
+
+    generateSalt: () => {
+        return crypto.randomBytes(18 / 2).toString('hex').slice(0, 18)
+    },
+    setPassword: (password, salt) => {
+        let hash = crypto.createHmac('sha512', salt)
         hash.update(password)
-        let value=hash.digest('hex')
-        return{
-            salt:salt,
-            passwordHash:value
+        let value = hash.digest('hex')
+        return {
+            salt: salt,
+            passwordHash: value
         }
     }
 }
