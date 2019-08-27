096ec9ddbd1da0511a78dba4bd703cbeab2c6713
falihnaufal17 <dudu.kotkot@gmail.com>
2019-08-27 11:10:31 +0700

    CRUD Sub Category

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const userRoutes = require('./src/routes/user')
+const subCategoryRoutes = require('./src/routes/subCategory')
 const port = 3400
-app.use(bodyParser.urlencoded({extended:false}))
-app.listen(port,()=>{
+app.use(bodyParser.urlencoded({ extended: false }))
+app.listen(port, () => {
     console.log(`We are running in port ${port}`);
 })
 
-app.use('/user',userRoutes)
+app.use('/user', userRoutes)
+app.use('/subCategory', subCategoryRoutes)
