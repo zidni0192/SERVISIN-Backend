397bfa716be55376d20b5d35c34764b6598bddd6
sluxz3r <rurul161011@gmail.com>
2019-08-27 13:12:11 +0700

    test

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,15 +1,16 @@
 const crypto = require('crypto')
 module.exports = {
-    generateSalt: () => {
-        return crypto.randomBytes(18/2).toString('hex').slice(0,18)
+    generateSalt: (length) => {
+        return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length)
     },
-    setPassword: (password,salt) =>{
-        let hash = crypto.createHmac('sha512',salt)
+
+    setPassword: (password, salt) => {
+        let hash = crypto.createHmac('sha512', salt)
         hash.update(password)
-        let value=hash.digest('hex')
-        return{
-            salt:salt,
-            passwordHash:value
+        let value = hash.digest('hex')
+        return {
+            salt: salt,
+            passwordHash: value
         }
     }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Route = express.Router()
 const controller = require('../controllers/user')
 Route
-.post('/register',controller.postUser)
-.post('/login',controller.getByEmail)
+    .post('/register', controller.postUser)
+    .post('/login', controller.getByEmail)
 
 module.exports = Route
